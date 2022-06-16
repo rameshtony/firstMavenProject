22a80f5abd2d192ec4e5824bdcb91b219b4a9bdf
shiva <shiva.nelikanti@gmail.com>
2022-06-16 14:51:57 +0530

    Shiva : June 16 2022: Adding project first time to github

---
+++ src/main/java/GroupIDTeamSixMaven/ArtifactIDTeamSixMaven/App.java
@@ -0,0 +1,13 @@
+package GroupIDTeamSixMaven.ArtifactIDTeamSixMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
