e761966e0be910093174e91a075507aa60c6e35d
ERUMALLA RAMESH <107615648+rameshtony@users.noreply.github.com>
2022-06-23 12:37:33 +0530

    Update App.java

--- src/main/java/GroupIDTeamSixMaven/ArtifactIDTeamSixMaven/App.java
+++ src/main/java/GroupIDTeamSixMaven/ArtifactIDTeamSixMaven/App.java
@@ -16,7 +16,6 @@ public class App
     	String str2 = "XXXX123Sh4G21";
 
 		String digits = str2.replaceAll("[a-z]", "");
-		System.out.println("adding from github place");
 		System.out.println(digits);
 		
     }
